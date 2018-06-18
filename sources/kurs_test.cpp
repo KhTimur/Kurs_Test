849d77414ac6f822201b1e21e81d5abd6edbf4bc
KhTimur <khazirov@yandex.ru>
2018-06-18 20:17:27 +0300

    Update kurs_test.cpp

--- sources/kurs_test.cpp
+++ sources/kurs_test.cpp
@@ -70,11 +70,11 @@ void downHeap(int * t, int * en) {
         int a=std::distance(t, k);
         if(*k> *(k+a))
             {
-                swap(*k,*(k+a));
+                std::swap(*k,*(k+a));
             }
         if(*k>*(k+a+1) && (k+a+1)!=en)
             {
-                swap(*k,*(k+a+1));
+                std::swap(*k,*(k+a+1));
             }
         k--;
     }
