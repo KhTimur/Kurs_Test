827a98226026d20dfaf35d7a3651dfa4932e3d00
KhTimur <khazirov@yandex.ru>
2018-06-19 23:26:36 +0300

    Update testsortmerge.cpp

--- tests/testsortmerge.cpp
+++ tests/testsortmerge.cpp
@@ -5,7 +5,10 @@ SCENARIO("SORT") {
 	int nums3[] = { 9,1,7,2,5,8,3,6,4 };
 	int nums_proverka3[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 	merge_sort(nums3, nums3+9);
+	int j=0;
 	for (int i = 0; i < 9; ++i) {
-		REQUIRE(nums3[i] == nums_proverka3[i]);
+		if(nums3[i] == nums_proverka3[i])
+			j++;
 	}
+	REQUIRE(j==9);
 }
